16168d1858889343010b05a372ac3cd45d338d15
wbeange <willbeange@gmail.com>
2014-08-17 16:14:48 -0700

    cleaning up, working state

--- ver-angularjs/js/app.js
+++ ver-angularjs/js/app.js
@@ -6,14 +6,14 @@
 
   TODO:
 
-  fix insta auth redir after timeout
   fix insta auth routing issues
-  friendship toggle
-  menu
+  friendship toggle serverside call
   error handling / max requests handling
-  user div styling - maybe small with hover
-  switch button on hover might look nicer
-  link to users profile on click in new window  
+  user div styling
+    maybe small with hover
+    buttons consistent size
+    no underline / blue anchor test for username
+    switch button on hover might look nicer
 
   */
 
@@ -42,16 +42,14 @@
 
 	app.controller('UsersCtrl', ['$scope', 'Instagram', '$http', '$window', '$cookieStore', '$location', function($scope, Instagram, $http, $window, $cookieStore, $location) {
 
-    var access_code = $cookieStore.get("accessCode");
-
+    var accessCode = $cookieStore.get("accessCode");
     var url = $location.url();
 
-    //console.log('url - ' + url);
-
-    if(access_code === undefined || url.indexOf('/access_token=') == -1)
+    //TODO: This needs to be somewhere else...
+    if(accessCode === undefined && url.indexOf('access_token') == -1)
     {
       //first we need to set the cookie so that we can get the access token :)
-      var redirectUri = 'http://localhost/#/';
+      var redirectUri = 'http://localhost/';
 
       var authorization_url = 'https://instagram.com/oauth/authorize/';
       authorization_url += '?client_id=0ed0e250ea854a129e9a849a8ee0ed9c';  
@@ -63,115 +61,119 @@
     }
     else
     {
-      var accessCode = url.replace('/access_token=', '');
-      $cookieStore.put("accessCode", accessCode);
-      $location.url('/');
+      if(url.indexOf('/access_token=') != -1)
+      {
+        var accessCode = url.replace('/access_token=', '');
+        $cookieStore.put("accessCode", accessCode);
 
-    //TODO ^^^ Fix Auth
+        $location.url('/');
+      }
 
-    $scope.toggleRelationship = function(user) {
+      //TODO ^^^ Fix Auth
 
-      //TODO: Note - it looks like it will carry accross other array. cool.
+      $scope.toggleRelationship = function(user) {
 
-      _.each($scope.users, function(u) {
-        if(u.id == user.id)
-        {          
-          u.follow = !u.follow;
-        }          
-      });
+        //TODO: Note - it looks like it will carry accross other array. cool.
 
-    };
+        _.each($scope.users, function(u) {
+          if(u.id == user.id)
+          {          
+            u.follow = !u.follow;
+          }          
+        });
 
-    $scope.loadUsers = function(groupId) {
-			if(groupId === 'follows')
-			{
-				$scope.users = $scope.follows;
-				$scope.usersCount = $scope.followsCount;
-			}
-			else if(groupId === 'followedBy')
-			{
-				$scope.users = $scope.followedBy;
-				$scope.usersCount = $scope.followedByCount;
-			}
-			else if(groupId === 'fans')
-			{
-				$scope.users = $scope.fans;
-				$scope.usersCount = $scope.fansCount;
-			}
-			else //if(groupId === 'idols')
-			{
-				$scope.users = $scope.idols;
-				$scope.usersCount = $scope.idolsCount;
-			}
-		};
+      };
 
-		//init variables
-		$scope.users = [];
-		$scope.usersCount = 0;
+      $scope.loadUsers = function(groupId) {
+  			if(groupId === 'follows')
+  			{
+  				$scope.users = $scope.follows;
+  				$scope.usersCount = "You follow " + $scope.followsCount + " people.";
+  			}
+  			else if(groupId === 'followedBy')
+  			{
+  				$scope.users = $scope.followedBy;
+  				$scope.usersCount = $scope.followedByCount + " people follow you.";
+  			}
+  			else if(groupId === 'fans')
+  			{
+  				$scope.users = $scope.fans;
+  				$scope.usersCount = $scope.fansCount + " people follow you that you don't follow back.";
+  			}
+  			else //if(groupId === 'idols')
+  			{
+  				$scope.users = $scope.idols;
+  				$scope.usersCount = "You follow " + $scope.idolsCount + " people that don't follow you back.";
+  			}
+  		};
 
-		$scope.follows = [];
-		$scope.followsCount = 0;
+  		//init variables
+  		//$scope.users = [];
+  		//$scope.usersCount = 0;
 
-		$scope.followedBy = [];
-		$scope.followedByCount = 0;
+  		$scope.follows = [];
+  		$scope.followsCount = 0;
 
-		$scope.fans = [];
-		$scope.fansCount = 0;
+  		$scope.followedBy = [];
+  		$scope.followedByCount = 0;
 
-		$scope.idols = [];
-		$scope.idolsCount = 0;
+  		$scope.fans = [];
+  		$scope.fansCount = 0;
 
-		/* $scope.users = [{
-				username: "xococoho",
-				bio: "Professional Surfer from Hawaii 🐬 (everything I wear is @VolcomWomens) Trust.Honor.Loyalty.Love",
-				website: "http://www.usatoday.com/media/cinematic/video/13364719/surfer-coco-ho-on-appearing-in-espn-body-issue/",
-				profile_picture: "http://photos-c.ak.instagram.com/hphotos-ak-xaf1/10601766_747458748625730_1526191873_a.jpg",
-				full_name: "Coco Ho",
-				id: "10908579"				
-			}]; */
+  		$scope.idols = [];
+  		$scope.idolsCount = 0;
 
-		//load variables
-		Instagram.getRelationshipData().then(function(data) {
-			follows = data[0];
-			followedBy = data[1];
+  		/* $scope.users = [{
+  				username: "xococoho",
+  				bio: "Professional Surfer from Hawaii 🐬 (everything I wear is @VolcomWomens) Trust.Honor.Loyalty.Love",
+  				website: "http://www.usatoday.com/media/cinematic/video/13364719/surfer-coco-ho-on-appearing-in-espn-body-issue/",
+  				profile_picture: "http://photos-c.ak.instagram.com/hphotos-ak-xaf1/10601766_747458748625730_1526191873_a.jpg",
+  				full_name: "Coco Ho",
+  				id: "10908579"				
+  			}]; */
 
-      var fans = findDifference(followedBy, follows);
-      var idols = findDifference(follows, followedBy);
+  		//load variables
+  		Instagram.getRelationshipData().then(function(data) {
+  			follows = data[0];
+  			followedBy = data[1];
 
-      _.each(follows, function(u) {
-        u.follow = true;
-      });
+        var fans = findDifference(followedBy, follows);
+        var idols = findDifference(follows, followedBy);
 
-      _.each(followedBy, function(u) {
-        if(_.has(fans, u.id) === true)
-          u.follow = false;
-        else
+        _.each(follows, function(u) {
           u.follow = true;
-      });
+        });
 
-      _.each(fans, function(u) {
-        u.follow = false;
-      });
+        _.each(followedBy, function(u) {
+          if(_.has(fans, u.id) === true)
+            u.follow = false;
+          else
+            u.follow = true;
+        });
 
-      _.each(idols, function(u) {
-        u.follow = true;
-      });
+        _.each(fans, function(u) {
+          u.follow = false;
+        });
 
-			$scope.follows = follows;
-			$scope.followsCount = follows.length;
+        _.each(idols, function(u) {
+          u.follow = true;
+        });
 
-			$scope.followedBy = followedBy;
-			$scope.followedByCount = followedBy.length;
+  			$scope.follows = follows;
+  			$scope.followsCount = follows.length;
 
-			$scope.fans = fans;
-			$scope.fansCount = _.size(fans);
+  			$scope.followedBy = followedBy;
+  			$scope.followedByCount = followedBy.length;
 
-			$scope.idols = idols;
-			$scope.idolsCount = _.size(idols);
+  			$scope.fans = fans;
+  			$scope.fansCount = _.size(fans);
 
-			$scope.loadUsers('follows');
+  			$scope.idols = idols;
+  			$scope.idolsCount = _.size(idols);
 
-		});
+  			$scope.loadUsers('follows');
+
+  		});
 
     }
 
@@ -228,9 +230,7 @@
     function getFollows() {            
       var accessCode = $cookieStore.get("accessCode");
       var userId = '183356248';
-      var base = 'https://api.instagram.com/v1/users/' + userId + '/follows';
-      var access_token = '?access_token=' + accessCode + '&callback=JSON_CALLBACK';
-      var url = base + access_token;
+      var url = 'https://api.instagram.com/v1/users/' + userId + '/follows' + '?access_token=' + accessCode + '&callback=JSON_CALLBACK';
 
       return _getUsers(_getUsers, url, '', []);
     }
@@ -238,9 +238,7 @@
     function getFollowedBy() {
       var accessCode = $cookieStore.get("accessCode");
       var userId = '183356248';
-      var base = 'https://api.instagram.com/v1/users/' + userId + '/followed-by';
-      var access_token = '?access_token=' + accessCode + '&callback=JSON_CALLBACK';
-      var url = base + access_token;
+      var url = 'https://api.instagram.com/v1/users/' + userId + '/followed-by' + '?access_token=' + accessCode + '&callback=JSON_CALLBACK';
 
       return _getUsers(_getUsers, url, '', []);
     }
@@ -248,9 +246,9 @@
     //Recursively fetch users from Instagram API
     function _getUsers(callback_fn, url, cursor, users) {
 
-      var full_url = url + cursor;
+      var fullUrl = url + cursor;
 
-      return $http.jsonp(full_url).then(
+      return $http.jsonp(fullUrl).then(
         //success
         function(result) {
 
