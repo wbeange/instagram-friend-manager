c265842c2fb7a30697d3606cfcf8ab45f5447398
wbeange <willbeange@gmail.com>
2014-08-17 16:17:05 -0700

    indents to spaces

--- ver-angularjs/js/app.js
+++ ver-angularjs/js/app.js
@@ -40,7 +40,7 @@
 
   }]); */
 
-	app.controller('UsersCtrl', ['$scope', 'Instagram', '$http', '$window', '$cookieStore', '$location', function($scope, Instagram, $http, $window, $cookieStore, $location) {
+  app.controller('UsersCtrl', ['$scope', 'Instagram', '$http', '$window', '$cookieStore', '$location', function($scope, Instagram, $http, $window, $cookieStore, $location) {
 
     var accessCode = $cookieStore.get("accessCode");
     var url = $location.url();
@@ -85,57 +85,57 @@
       };
 
       $scope.loadUsers = function(groupId) {
-  			if(groupId === 'follows')
-  			{
-  				$scope.users = $scope.follows;
-  				$scope.usersCount = "You follow " + $scope.followsCount + " people.";
-  			}
-  			else if(groupId === 'followedBy')
-  			{
-  				$scope.users = $scope.followedBy;
-  				$scope.usersCount = $scope.followedByCount + " people follow you.";
-  			}
-  			else if(groupId === 'fans')
-  			{
-  				$scope.users = $scope.fans;
-  				$scope.usersCount = $scope.fansCount + " people follow you that you don't follow back.";
-  			}
-  			else //if(groupId === 'idols')
-  			{
-  				$scope.users = $scope.idols;
-  				$scope.usersCount = "You follow " + $scope.idolsCount + " people that don't follow you back.";
-  			}
-  		};
+        if(groupId === 'follows')
+        {
+          $scope.users = $scope.follows;
+          $scope.usersCount = "You follow " + $scope.followsCount + " people.";
+        }
+        else if(groupId === 'followedBy')
+        {
+          $scope.users = $scope.followedBy;
+          $scope.usersCount = $scope.followedByCount + " people follow you.";
+        }
+        else if(groupId === 'fans')
+        {
+          $scope.users = $scope.fans;
+          $scope.usersCount = $scope.fansCount + " people follow you that you don't follow back.";
+        }
+        else //if(groupId === 'idols')
+        {
+          $scope.users = $scope.idols;
+          $scope.usersCount = "You follow " + $scope.idolsCount + " people that don't follow you back.";
+        }
+      };
 
-  		//init variables
-  		//$scope.users = [];
-  		//$scope.usersCount = 0;
+      //init variables
+      //$scope.users = [];
+      //$scope.usersCount = 0;
 
-  		$scope.follows = [];
-  		$scope.followsCount = 0;
+      $scope.follows = [];
+      $scope.followsCount = 0;
 
-  		$scope.followedBy = [];
-  		$scope.followedByCount = 0;
+      $scope.followedBy = [];
+      $scope.followedByCount = 0;
 
-  		$scope.fans = [];
-  		$scope.fansCount = 0;
+      $scope.fans = [];
+      $scope.fansCount = 0;
 
-  		$scope.idols = [];
-  		$scope.idolsCount = 0;
+      $scope.idols = [];
+      $scope.idolsCount = 0;
 
-  		/* $scope.users = [{
-  				username: "xococoho",
-  				bio: "Professional Surfer from Hawaii 🐬 (everything I wear is @VolcomWomens) Trust.Honor.Loyalty.Love",
-  				website: "http://www.usatoday.com/media/cinematic/video/13364719/surfer-coco-ho-on-appearing-in-espn-body-issue/",
-  				profile_picture: "http://photos-c.ak.instagram.com/hphotos-ak-xaf1/10601766_747458748625730_1526191873_a.jpg",
-  				full_name: "Coco Ho",
-  				id: "10908579"				
-  			}]; */
+      /* $scope.users = [{
+          username: "xococoho",
+          bio: "Professional Surfer from Hawaii 🐬 (everything I wear is @VolcomWomens) Trust.Honor.Loyalty.Love",
+          website: "http://www.usatoday.com/media/cinematic/video/13364719/surfer-coco-ho-on-appearing-in-espn-body-issue/",
+          profile_picture: "http://photos-c.ak.instagram.com/hphotos-ak-xaf1/10601766_747458748625730_1526191873_a.jpg",
+          full_name: "Coco Ho",
+          id: "10908579"        
+        }]; */
 
-  		//load variables
-  		Instagram.getRelationshipData().then(function(data) {
-  			follows = data[0];
-  			followedBy = data[1];
+      //load variables
+      Instagram.getRelationshipData().then(function(data) {
+        follows = data[0];
+        followedBy = data[1];
 
         var fans = findDifference(followedBy, follows);
         var idols = findDifference(follows, followedBy);
@@ -159,69 +159,69 @@
           u.follow = true;
         });
 
-  			$scope.follows = follows;
-  			$scope.followsCount = follows.length;
+        $scope.follows = follows;
+        $scope.followsCount = follows.length;
 
-  			$scope.followedBy = followedBy;
-  			$scope.followedByCount = followedBy.length;
+        $scope.followedBy = followedBy;
+        $scope.followedByCount = followedBy.length;
 
-  			$scope.fans = fans;
-  			$scope.fansCount = _.size(fans);
+        $scope.fans = fans;
+        $scope.fansCount = _.size(fans);
 
-  			$scope.idols = idols;
-  			$scope.idolsCount = _.size(idols);
+        $scope.idols = idols;
+        $scope.idolsCount = _.size(idols);
 
-  			$scope.loadUsers('follows');
+        $scope.loadUsers('follows');
 
-  		});
+      });
 
     }
 
-		//find difference in arrays
-		var findDifference = function(usersArray1, usersArray2) {			
-			var differenceKeyValue = {};
+    //find difference in arrays
+    var findDifference = function(usersArray1, usersArray2) {     
+      var differenceKeyValue = {};
 
-			//check if there is a mutual 'friends' relationship
-			//ie user exists in both arrays
-			for(var i=0; i<usersArray1.length; i++)
-			{
-				var friends = false;
+      //check if there is a mutual 'friends' relationship
+      //ie user exists in both arrays
+      for(var i=0; i<usersArray1.length; i++)
+      {
+        var friends = false;
 
-				for(var j=0; j<usersArray2.length; j++)
-				{
-					if(usersArray1[i].id == usersArray2[j].id)
-					{
-						friends = true;
-						continue;
-					}
-				}
+        for(var j=0; j<usersArray2.length; j++)
+        {
+          if(usersArray1[i].id == usersArray2[j].id)
+          {
+            friends = true;
+            continue;
+          }
+        }
 
-				if(friends === false)
-				{
+        if(friends === false)
+        {
           //differenceArray.push(usersArray1[i]);
-					differenceKeyValue[ usersArray1[i].id ] = usersArray1[i];
-				}
-			}
-			
-			return differenceKeyValue;
-		};
+          differenceKeyValue[ usersArray1[i].id ] = usersArray1[i];
+        }
+      }
+      
+      return differenceKeyValue;
+    };
 
-	}]);
+  }]);
 
-	//Communicates with Server
-	app.service('Instagram', ['$http', '$q', '$cookieStore', function($http, $q, $cookieStore) {
-		
-		//
+  //Communicates with Server
+  app.service('Instagram', ['$http', '$q', '$cookieStore', function($http, $q, $cookieStore) {
+    
+    //
     //Public
     //
 
     this.getRelationshipData = function() {
 
-			return $q.all([
-				getFollows(),
-				getFollowedBy()
-				]);
-		}
+      return $q.all([
+        getFollows(),
+        getFollowedBy()
+        ]);
+    }
 
     //
     //Private
@@ -269,6 +269,6 @@
         }
       )};
 
-	}]);
+  }]);
 
 })();
