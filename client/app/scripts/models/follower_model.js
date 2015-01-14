c028ad8038a2839affc6f300864e5cccf3f4c5d5
Will Beange <will@convertstar.com>
2015-01-14 14:55:02 -0800

    recursive promise working, able to get all followers from instagram api with jsonp, passive auth

--- client/app/scripts/models/follower_model.js
+++ client/app/scripts/models/follower_model.js
@@ -14,31 +14,47 @@ angular.module('clientApp').factory('FollowerModel', function($q, $http, Auth) {
   // public
   //
 
-  FollowerModel.prototype.all = function(userId) {    
-    var url = 'https://api.instagram.com/v1/users/' + userId + '/follows' + '?access_token=' + Auth.accessToken() + '&callback=JSON_CALLBACK';
+  var cursor = '',
+      followers = [],
+      url;
 
-    return this.recursiveAll(url);
+  FollowerModel.prototype.all = function(userId) {    
+    url = 'https://api.instagram.com/v1/users/' + userId + '/follows' + '?access_token=' + Auth.accessToken() + '&callback=JSON_CALLBACK';
+    cursor = '';
+    followers = [];
+
+    return this.recursiveAll();
   }
 
-  FollowerModel.prototype.recursiveAll = function(url) {
-    var deferred = $q.defer();
+  FollowerModel.prototype.recursiveAll = function(deferred) {
+    if(deferred === undefined) {
+      var deferred = $q.defer();
+    }
+    
+    var fullUrl = url + cursor;
 
     var self = this;
 
-    $http.jsonp(url).then(function(results) {
-      console.log('FollowerModel.all()', results);
+    $http.jsonp(fullUrl).then(
+      // success
+      function(results) {    
+        // build return array
+        followers = followers.concat(results.data.data);
 
-      // if(results.data.pagination.next_cursor) {
-      //   return self.
-      // }
+        if(results.data.pagination.next_cursor) {
+          cursor = '&cursor=' + results.data.pagination.next_cursor;
 
-      // double data.data because of jsonp return
-      deferred.resolve(results.data.data);
-    },
+          self.recursiveAll(deferred);
+        } else {
+          // double data.data because of jsonp return
+          deferred.resolve(followers);
+        }      
+      },
 
-    function(results) {
-      deferred.reject(results);
-    });
+      // error
+      function() {
+        deferred.reject();
+      });
 
     return deferred.promise;
   }
