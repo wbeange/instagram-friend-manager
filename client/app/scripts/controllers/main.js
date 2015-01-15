5cd90e575cf2d1e19985f1f56814f522cc809160
Will Beange <will@convertstar.com>
2015-01-14 17:00:38 -0800

    added fans model, it works

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -8,16 +8,20 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .controller('MainCtrl', function ($scope, UserModel, FollowModel, FollowedByModel) {
+  .controller('MainCtrl', function ($scope, UserModel, FollowModel, FollowedByModel, FanModel) {
     UserModel.get().then(function(data) {
       var userId = data.id;
 
-      FollowModel.all(userId).then(function(follows) {
-        $scope.follows = follows;
-      });
+      // FollowModel.all(userId).then(function(follows) {
+      //   $scope.follows = follows;
+      // });
 
-      FollowedByModel.all(userId).then(function(followedBys) {
-        $scope.followedBys = followedBys;
+      // FollowedByModel.all(userId).then(function(followedBys) {
+      //   $scope.followedBys = followedBys;
+      // });
+
+      FanModel.all(userId).then(function(fans) {
+        $scope.follows = fans;
       });
     });
   });
