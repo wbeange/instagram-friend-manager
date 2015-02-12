9dfb8b9af83cdd7bca7b5bb5647b85a983d66870
Will Beange <will@convertstar.com>
2015-02-11 22:00:01 -0800

    moved popover view to template cache

--- client/app/scripts/directives/wb_user_popover_directive.js
+++ client/app/scripts/directives/wb_user_popover_directive.js
@@ -1,4 +1,4 @@
- angular.module('clientApp').directive('wbUserPopover', function($compile, $timeout) {
+ angular.module('clientApp').directive('wbUserPopover', function($compile, $timeout, $interpolate, $templateCache) {
   return {
     restrict: "A",
     replace: true,
@@ -9,29 +9,11 @@
     link: function(scope, element, attrs) {
       var elId = 'user_popover_' + scope.user.id;
 
-      // TODO - template this
-      var html = ' \
-        <div id="' + elId + '" style="width:250px;"> \
-          <div class="quarter-page top"> \
-            <div style="margin-left:auto;margin-right:auto;padding-top:3px;"> \
-              <img class="ui-corner-all img-rounded" ng-src="{{user.profile_picture}}" alt="..." style="width:100%;"> \
-            </div> \
-          </div><div class="three-quarter-page top"> \
-            <div class="padding-left-3">{{user.full_name}}</div> \
-            <div class="padding-left-3">@{{user.username}}</div> \
-          </div> \
-          \
-          <div>Web: {{user.website}}</div> \
-          <div>Bio: {{user.bio}}</div> \
-          \
-          <div class="third-page top center"> \
-            photos \
-          </div><div class="third-page top center"> \
-            following \
-          </div><div class="third-page top center"> \
-            followers \
-          </div> \
-        </div>';
+      // view in template
+      var html = $templateCache.get('user_popup_template');
+
+      // pop var w/ interpolate
+      html = $interpolate(html)({ elId: elId, user: scope.user });
 
       // initialize Javascript Bootstrap Popover library element
       $( element ).popover({
@@ -41,7 +23,7 @@
         content: html
       });
 
-      // compile html on hover
+      // compile html on hover for ng-src directive, etc...
       $( element ).mouseenter(function() {
         $compile( $('#'+elId).contents() )(scope); 
       });
