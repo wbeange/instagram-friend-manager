c1466f8ec4e43df02284184e8f45bfecc2cd8939
Will Beange <will@convertstar.com>
2015-07-13 13:41:24 -0700

    implemented a better back end

--- server/instagram_friend_manager_app.rb
+++ server/instagram_friend_manager_app.rb
@@ -7,19 +7,43 @@
 
 require "sinatra"
 require "sinatra/config_file"
+require "sinatra/json"
 require "instagram"
-
-config_file 'config.yml'
+require "json"
 
 enable :sessions
 
-CALLBACK_URL = settings.callback_url
+# config file holds instagram app api settings
+config_file 'config.yml'
 
-Instagram.configure do |config|
-  config.client_id = settings.client_id
-  config.client_secret = settings.client_secret
-  # For secured endpoints only
-  #config.client_ips = '<Comma separated list of IPs>'
+# enable CORDS
+before do
+   headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+   headers['Access-Control-Allow-Origin'] = 'http://localhost:9001'
+   headers['Access-Control-Allow-Headers'] = 'accept, authorization, origin'
+   headers['Access-Control-Allow-Credentials'] = 'true'
+end
+
+# ensure authenticated
+before do
+
+  # TODO: which need to be passed over?
+  pass if request.path_info == "/oauth/connect"
+  pass if request.path_info == "/oauth/disconnect"
+  pass if request.path_info == "/oauth/callback"
+
+  # ensure logged in
+  redirect "/oauth/connect" unless session[:access_token]
+end
+
+before do
+  # init ruby instagram api
+  Instagram.configure do |config|
+    config.client_id = settings.client_id
+    config.client_secret = settings.client_secret
+  end
+
+  @client = Instagram.client(:access_token => session[:access_token])
 end
 
 get "/" do
@@ -27,34 +51,32 @@ get "/" do
 end
 
 get "/oauth/connect" do
-  redirect Instagram.authorize_url(:redirect_uri => CALLBACK_URL)
+  redirect Instagram.authorize_url(:redirect_uri => settings.callback_url)
+end
+
+# TODO: make this a DELETE request
+get "/oauth/disconnect" do
+  session[:access_token] = nil
+  status 200
 end
 
 get "/oauth/callback" do
-  content_type :json
-  response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
+  response = Instagram.get_access_token(params[:code], :redirect_uri => settings.callback_url)
   session[:access_token] = response.access_token
-  {:status => 200}.to_json
+  {:status => 200, :data => {:code => params[:code]}}.to_json
 end
 
 get "/users/self" do
-  content_type :json
-  client = Instagram.client(:access_token => session[:access_token])
-  client.user.to_json
+  json @client.user
 end
 
 get "/users/:user_id/follows" do
-  content_type :json
-  client = Instagram.client(:access_token => session[:access_token])
-  client.user_follows.to_json
+  client =
+  json client.user_follows
 end
 
 get "/limits" do
-  # content_type :json
-  client = Instagram.client(:access_token => session[:access_token])
-  response = client.utils_raw_response
-
+  response = @client.utils_raw_response
   limit = response.headers[:x_ratelimit_limit]
-  # {:limit => limit}.to_json
-  limit
+  {:limit => limit}.to_json
 end
