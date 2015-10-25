64a5032bff119f4f17397c3fc715e22edd5c4f20
Will Beange <will@convertstar.com>
2015-10-25 16:47:55 -0700

    fixed the user route and user caching

--- client/app/scripts/services/configuration_service.js
+++ client/app/scripts/services/configuration_service.js
@@ -4,11 +4,11 @@ angular.module('clientApp').factory('Configuration', function() {
 
   return {
     // TODO: handle dev / production variables in grunt
-    // baseApiUrl: 'http://localhost:4567',
-    baseApiUrl: 'https://api-instagram-friend-manager.herokuapp.com',
+    baseApiUrl: 'http://localhost:4567',
+    // baseApiUrl: 'https://api-instagram-friend-manager.herokuapp.com',
 
-    // baseClientUrl: 'http://localhost:9001',
-    baseClientUrl: 'https://insta-friend-manager.firebaseapp.com',
+    baseClientUrl: 'http://localhost:9001',
+    // baseClientUrl: 'https://insta-friend-manager.firebaseapp.com',
 
     clientSessionKey: 'insta-friend-mngr'
   };
